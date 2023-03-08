3cc484208d1e884e7b8be768fb9b2bc480e9a3f9
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-09 01:18:15 +0200

    Create an Add icon

--- sampleprojecject/lib/view/notes_view.dart
+++ sampleprojecject/lib/view/notes_view.dart
@@ -1,3 +1,4 @@
+import 'dart:js_util';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -9,8 +10,12 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: NotesViewBody(),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      body: const NotesViewBody(),
     );
   }
 }
