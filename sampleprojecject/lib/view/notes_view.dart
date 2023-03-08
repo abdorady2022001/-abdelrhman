9107bfbb551d016179fbe60cff6110c561d6902d
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-09 01:46:09 +0200

    Create Add page in home page (showModalBottomSheet)

--- sampleprojecject/lib/view/notes_view.dart
+++ sampleprojecject/lib/view/notes_view.dart
@@ -12,10 +12,25 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return Container();
+              });
+        },
         child: const Icon(Icons.add),
       ),
       body: const NotesViewBody(),
     );
+  }
+}
+
+class AddNoteBottomSheet extends StatelessWidget {
+  const AddNoteBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
