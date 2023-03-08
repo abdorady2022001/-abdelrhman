cf858573e1c77bf789d7dea66bb1e7bc6172ebcc
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-08 21:06:53 +0200

    Create a place to search and control from his place

--- sampleprojecject/lib/view/widgets/costom_app_bar.dart
+++ sampleprojecject/lib/view/widgets/costom_app_bar.dart
@@ -12,8 +12,30 @@ class CutomAppBar extends StatelessWidget {
           style: TextStyle(
             fontSize: 28,
           ),
-        )
+        ),
+        Spacer(),
+        CustomSearchIon(),
       ],
+    );
+  }
+}
+
+class CustomSearchIon extends StatelessWidget {
+  const CustomSearchIon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 45,
+      width: 45,
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Center(
+        child: Icon(
+          Icons.search,
+        ),
+      ),
     );
   }
 }
