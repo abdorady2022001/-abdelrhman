f180bc641d2fa32e5811e975e18b41ed67e82dc1
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-09 03:59:38 +0200

    Create an add button and change the font color

--- sampleprojecject/lib/view/widgets/add_note_bottom_sheet.dart
+++ sampleprojecject/lib/view/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sampleprojecject/constants.dart';
 import 'custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -23,7 +24,34 @@ class AddNoteBottomSheet extends StatelessWidget {
             hint: 'content',
             maxlines: 5,
           ),
+          custombutton(),
         ],
+      ),
+    );
+  }
+}
+
+class custombutton extends StatelessWidget {
+  const custombutton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      decoration: BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.circular(
+            8,
+          )),
+      child: const Center(
+        child: Text(
+          'Add',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+          ),
+        ),
       ),
     );
   }
