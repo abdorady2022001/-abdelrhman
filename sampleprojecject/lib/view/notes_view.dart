4afd5f25c1a4938bd99e94074c7bed0f92dd9708
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-09 02:26:16 +0200

    Change the angle shape within the bottom sheet

--- sampleprojecject/lib/view/notes_view.dart
+++ sampleprojecject/lib/view/notes_view.dart
@@ -2,6 +2,7 @@ import 'dart:js_util';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:sampleprojecject/view/widgets/add_note_bottom_sheet.dart';
 
 import 'widgets/notes_view_body.dart';
 
@@ -14,23 +15,16 @@ class NotesView extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16)),
               context: context,
               builder: (context) {
-                return Container();
+                return const AddNoteBottomSheet();
               });
         },
         child: const Icon(Icons.add),
       ),
       body: const NotesViewBody(),
     );
-  }
-}
-
-class AddNoteBottomSheet extends StatelessWidget {
-  const AddNoteBottomSheet({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
