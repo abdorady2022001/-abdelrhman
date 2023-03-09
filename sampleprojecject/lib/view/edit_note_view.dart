e0ba013424fb2f28330e3c8df8c4a1259dc9f110
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-09 04:58:05 +0200

    Build a note page

---
+++ sampleprojecject/lib/view/edit_note_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:sampleprojecject/view/widgets/edit_view_body.dart';
+
+class EditnoteView extends StatelessWidget {
+  const EditnoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: EditnoteView(),
+    );
+  }
+}
