2dfdbfa1327bf338ff6a23f9251bd203b357561c
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-09 04:58:30 +0200

    Build a note page

--- sampleprojecject/lib/view/widgets/custom_note_item.dart
+++ sampleprojecject/lib/view/widgets/custom_note_item.dart
@@ -1,49 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:sampleprojecject/view/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-        ListTile(
-          title: const Text(
-            'flutter Tips',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 26,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) {
+            return const EditnoteView();
+          }),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+          ListTile(
+            title: const Text(
+              'flutter Tips',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26,
+              ),
+            ),
+            subtitle: const Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 16),
+              child: Text(
+                'Buid your caeer with tharwat samy',
+                style: TextStyle(color: Colors.black54, fontSize: 18),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.black87,
+                size: 30,
+              ),
             ),
           ),
-          subtitle: const Padding(
-            padding: const EdgeInsets.only(top: 16, bottom: 16),
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
             child: Text(
-              'Buid your caeer with tharwat samy',
-              style: TextStyle(color: Colors.black54, fontSize: 18),
+              'May 12,2023',
+              style: TextStyle(color: Colors.black54, fontSize: 14),
             ),
           ),
-          trailing: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.delete,
-              color: Colors.black87,
-              size: 30,
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(right: 16),
-          child: Text(
-            'May 12,2023',
-            style: TextStyle(color: Colors.black54, fontSize: 14),
-          ),
-        ),
-      ]),
+        ]),
+      ),
     );
   }
 }
