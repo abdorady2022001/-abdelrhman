be8d123f9b8d2a8b0a1179b54d57daab729c2e90
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-09 04:28:35 +0200

    finsh notes bottom sheet

--- sampleprojecject/lib/view/widgets/add_note_bottom_sheet.dart
+++ sampleprojecject/lib/view/widgets/add_note_bottom_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sampleprojecject/constants.dart';
+import 'costom_button.dart';
 import 'custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -9,48 +10,30 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: const [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextField(
-            hint: 'titel',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            hint: 'content',
-            maxlines: 5,
-          ),
-          custombutton(),
-        ],
-      ),
-    );
-  }
-}
-
-class custombutton extends StatelessWidget {
-  const custombutton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      decoration: BoxDecoration(
-          color: kPrimaryColor,
-          borderRadius: BorderRadius.circular(
-            8,
-          )),
-      child: const Center(
-        child: Text(
-          'Add',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-          ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: const [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextField(
+              hint: 'titel',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hint: 'content',
+              maxlines: 5,
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            custombutton(),
+            SizedBox(
+              height: 15,
+            )
+          ],
         ),
       ),
     );
