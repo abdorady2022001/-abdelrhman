2d4de011fd05be4789c7f705dad9c86f9ef3d78e
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-03-09 03:18:24 +0200

    How to make the title icon and change the color and size

---
+++ sampleprojecject/lib/view/widgets/custom_text_field.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'package:flutter/material.dart';
+import 'package:sampleprojecject/constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+        hintText: 'title',
+        hintStyle: const TextStyle(color: kPrimaryColor),
+        border: buildborder(),
+        enabledBorder: buildborder(),
+        focusedBorder: buildborder(kPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildborder([Color]) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(
+          8,
+        ),
+        borderSide: BorderSide(color: Color ?? Colors.white));
+  }
+}
